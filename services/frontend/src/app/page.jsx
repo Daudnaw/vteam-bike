7328de83e866bae0cdb688e5c78fd012a9844bb8
David Hildingsson <david.hildingsson@tierset.se>
2025-12-12 09:56:11 +0100

    feat: add landingpage navigation

--- services/frontend/src/app/page.jsx
+++ services/frontend/src/app/page.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Page() {
-    // Check if session is active, redirect to user/admin dashboard or login route is no session is active
     return (
-        <div className='flex justify-center items-center h-screen flex-col gap-5'>
-            <h1 className='text-h1'>Hello world!</h1>
-            <Link href='/example' className='text-primary-dark text-h3'>
-                To example page!
-            </Link>
+        <div className='flex justify-center items-center h-screen relative'>
+            <Image
+                src='/scooters.png'
+                alt='Scooters'
+                fill={true}
+                className='opacity-10'
+            />
+            <div className='flex flex-col w-xl bg-primary-light p-5 rounded-md shadow-2xl z-10 border-primary-dark border-2'>
+                <h1 className='text-h1 font-bold text-center'>Välkommen</h1>
+                <p className='text-p mt-5 text-center'>
+                    Välj det alternativ som passar dig bäst!
+                </p>
+                <div className='flex mt-5 items-center gap-5 w-full'>
+                    <Link
+                        href='/app'
+                        className='  bg-primary-dark py-4 px-8 text-h4 font-bold text-white rounded-md flex-1 text-center hover:text-detail-yellow'
+                    >
+                        Användarapp
+                    </Link>
+                    <Link
+                        href='/webb'
+                        className=' bg-primary-dark py-4 px-8 text-h4 font-bold text-white rounded-md flex-1 text-center hover:text-detail-yellow'
+                    >
+                        Webbapp
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
