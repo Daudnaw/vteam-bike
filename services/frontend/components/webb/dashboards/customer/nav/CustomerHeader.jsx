67ca9d6c29c0366c9054d2091e4965e150aa0490
David Hildingsson <david.hildingsson@tierset.se>
2025-12-12 15:11:31 +0100

    fix: bg color dashboard header

--- services/frontend/components/webb/dashboards/customer/nav/CustomerHeader.jsx
+++ services/frontend/components/webb/dashboards/customer/nav/CustomerHeader.jsx
@@ -3,7 +3,7 @@ import { User } from 'lucide-react';
 
 export default function CustomerHeader({ email }) {
     return (
-        <div className='w-full border-b-2 h-10 flex items-center justify-end pr-5'>
+        <div className='w-full border-b-2 h-10 flex items-center justify-end pr-5 bg-primary'>
             <User className='mr-2 text-primary-dark' />
             <span>Hello, {email}</span>
         </div>
