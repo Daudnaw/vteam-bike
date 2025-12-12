79a285e7a4d90be491ffc417723ab0ddad9ce4b1
David Hildingsson <david.hildingsson@tierset.se>
2025-12-12 15:24:20 +0100

    feat: add login flow user app

--- services/frontend/components/userapp/auth/forms/LoginForm.jsx
+++ services/frontend/components/userapp/auth/forms/LoginForm.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Eye, EyeOff } from 'lucide-react';
-import { signin } from '../../../../src/app/actions/auth';
+import { signinapp } from '../../../../src/app/actions/auth';
 import Link from 'next/link';
 
 export default function LoginForm() {
@@ -13,7 +13,7 @@ export default function LoginForm() {
         try {
             setLoading(true);
             setError(null);
-            const res = await signin(formData);
+            const res = await signinapp(formData);
         } catch (err) {
             setError(err);
         }
--- services/frontend/src/app/(userapp)/app/page.jsx
+++ services/frontend/src/app/(userapp)/app/page.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { getSession } from '../../../../utils/user';
+import { redirect } from 'next/navigation';
 
-export default function Page() {
-    return <div>User app</div>;
+export default async function Page() {
+    const session = await getSession();
+
+    if (!session) {
+        redirect('/app/auth/login');
+    }
+
+    redirect('/app/user-app');
 }
