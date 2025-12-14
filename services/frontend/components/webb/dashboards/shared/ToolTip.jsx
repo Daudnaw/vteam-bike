a3dde11a1214338b5dd52f0575379c979f7eaf48
David Hildingsson <david.hildingsson@tierset.se>
2025-12-14 09:24:55 +0100

    fix: border on tooltip

--- services/frontend/components/webb/dashboards/shared/ToolTip.jsx
+++ services/frontend/components/webb/dashboards/shared/ToolTip.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ToolTip({ text }) {
     return (
-        <div className='w-fit bg-primary-light rounded-md shadow-2xl absolute right-0 -mr-[70px] z-50 p-2 text-base'>
+        <div className='w-fit bg-primary-light rounded-md shadow-2xl absolute right-0 -mr-[70px] z-50 p-2 text-base border-2 border-detail-yellow'>
             {text}
         </div>
     );
