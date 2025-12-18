6279449830c897a218de491557b48ed0cb25b1ff
David Hildingsson <david.hildingsson@tierset.se>
2025-12-18 21:26:29 +0100

    feat: add profile

--- services/frontend/src/app/(webb)/(user)/user-dashboard/profile/page.jsx
+++ services/frontend/src/app/(webb)/(user)/user-dashboard/profile/page.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import CustomerProfile from '../../../../../../components/webb/dashboards/customer/CustomerProfile';
+import { getSession } from '../../../../../../utils/user';
 
-export default function Page() {
-    return <div>Page</div>;
+export default async function Page() {
+    const session = await getSession();
+
+    return (
+        <div className='p-5'>
+            <h2 className='text-h2 mb-5 font-bold'>Profil</h2>
+
+            <CustomerProfile
+                firstName='Test'
+                lastName='Testsson'
+                email={session.email}
+                verified={false}
+            />
+        </div>
+    );
 }
