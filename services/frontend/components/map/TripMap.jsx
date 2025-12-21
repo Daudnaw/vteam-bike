c315fe40227419f2deee0c71e50f375fa0fc2216
David Hildingsson <david.hildingsson@tierset.se>
2025-12-21 14:53:11 +0100

    feat: add SingleTrip

---
+++ services/frontend/components/map/TripMap.jsx
@@ -0,0 +1,48 @@
+'use client';
+
+import 'leaflet/dist/leaflet.css';
+import {
+    MapContainer,
+    TileLayer,
+    Popup,
+    Marker,
+    Polyline,
+} from 'react-leaflet';
+import L from 'leaflet';
+import CenterTripMap from './CenterTripMap';
+
+const pinIcon = new L.Icon({
+    iconUrl: '/pin.svg',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+});
+
+export default function TripMap({ startPos, endPos }) {
+    return (
+        <MapContainer
+            scrollWheelZoom={false}
+            style={{ height: '600px', width: '100%' }}
+        >
+            <TileLayer
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                attribution='&copy; OpenStreetMap contributors'
+            />
+
+            <Marker icon={pinIcon} position={startPos}>
+                <Popup>Start</Popup>
+            </Marker>
+
+            <Marker icon={pinIcon} position={endPos}>
+                <Popup>Slut</Popup>
+            </Marker>
+
+            <Polyline
+                positions={[startPos, endPos]}
+                pathOptions={{ color: 'blue', weight: 2 }}
+            />
+
+            <CenterTripMap start={startPos} end={endPos} />
+        </MapContainer>
+    );
+}
---
+++ services/frontend/components/webb/dashboards/shared/SingleBill.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SingleBill() {
+    return <div>SingleBill</div>;
+}
