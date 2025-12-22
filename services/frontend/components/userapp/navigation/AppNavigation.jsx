181758c8df801b38598272713ecaf66211f7f43a
David Hildingsson <david.hildingsson@tierset.se>
2025-12-22 11:01:09 +0100

    fix: padding in app navigation

--- services/frontend/components/userapp/navigation/AppNavigation.jsx
+++ services/frontend/components/userapp/navigation/AppNavigation.jsx
@@ -6,25 +6,18 @@ export default function AppNavigation() {
     return (
         <div className='fixed bottom-0 flex bg-slate-800 text-white py-5 w-full px-4 z-1000'>
             <Link
-                className='border-r flex gap-2 border-detail-yellow flex-1 justify-center items-center pr-2'
-                href='/app/user-app/map'
-            >
-                <Map className='text-detail-yellow' />
-                <span>Karta</span>
-            </Link>
-            <Link
-                className='border-r border-detail-yellow flex gap-2 flex-1 justify-center items-center px-2'
-                href='/'
-            >
-                <Bike className='text-detail-yellow' />
-                <span>Scooter</span>
-            </Link>
-            <Link
-                className='border-r border-detail-yellow flex gap-2 flex-1 justify-center items-center px-2'
+                className='border-r border-detail-yellow flex gap-2 flex-1 justify-center items-center pr-2'
                 href='/'
             >
                 <Camera className='text-detail-yellow' />
                 <span>QR</span>
+            </Link>
+            <Link
+                className='border-r flex gap-2 border-detail-yellow flex-1 justify-center items-center px-2'
+                href='/app/user-app/map'
+            >
+                <Map className='text-detail-yellow' />
+                <span>Karta</span>
             </Link>
             <Link
                 className=' flex gap-2 flex-1 justify-center items-center pl-2'
