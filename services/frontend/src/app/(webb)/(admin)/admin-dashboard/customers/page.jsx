b2a7080455802901a6e2e8b0d9f9a48360d11b95
David Hildingsson <david.hildingsson@tierset.se>
2026-01-14 09:31:49 +0100

    feat: fix padding and layout admin customers

--- services/frontend/src/app/(webb)/(admin)/admin-dashboard/customers/page.jsx
+++ services/frontend/src/app/(webb)/(admin)/admin-dashboard/customers/page.jsx
@@ -4,9 +4,10 @@ import React from 'react';
 import UsersList from '../../../../../../components/webb/dashboards/admin/user/AllUser';
 
 export default function MyPage() {
-  return (
-    <section>
-      <UsersList />
-    </section>
-  );
+    return (
+        <div className='p-5'>
+            <h2 className='my-5 text-h2 font-bold'>Alla konton</h2>
+            <UsersList />
+        </div>
+    );
 }
