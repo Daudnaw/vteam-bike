2a90236ea528add4c49697faa91bae12a8a72b8b
Johan Guttormsson <655558+johangu@users.noreply.github.com>
2026-01-25 12:06:49 +0100

    fix: resolve exports

--- services/backend/modules/index.js
+++ services/backend/modules/index.js
@@ -2,6 +2,7 @@ import * as users from "./users/index.js";
 import * as auth from "./auth/index.js";
 import * as zones from "./zones/index.js";
 import * as admin from "./admin/index.js";
-import * as payments from "./payments/index.js"
+import * as payments from "./payments/index.js";
+import * as scooters from "./scooter/index.js";
 
-export const modules = [users, auth, zones, admin, payments];
+export const modules = [users, auth, zones, admin, payments, scooters];
