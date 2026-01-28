9996821c938de578d28199bf43d4920b6c945026
David Hildingsson <david.hildingsson@tierset.se>
2026-01-28 20:55:24 +0100

    feat: adding rentals to user webb

--- services/frontend/components/webb/dashboards/shared/SingleTrip.jsx
+++ services/frontend/components/webb/dashboards/shared/SingleTrip.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import TripMap from '../../../map/TripMap';
-import { Calendar, ArrowLeft } from 'lucide-react';
+import { Calendar, ArrowLeft, DollarSign } from 'lucide-react';
 import Link from 'next/link';
 
 export default function SingleTrip({ trip }) {
+    function formatTime(date) {
+        const newDate = new Date(date);
+
+        const formatDate =
+            newDate.getFullYear() +
+            '/' +
+            newDate.getMonth() +
+            1 +
+            '/' +
+            newDate.getDate();
+
+        return formatDate;
+    }
+
     return (
         <div className='p-5'>
-            <Link href='/user-dashboard'>
+            <Link href='/user-dashboard/trips'>
                 <ArrowLeft className='h-10 w-10 hover:text-detail-yellow' />
             </Link>
-            <h2 className='text-h2 font-bold mb-5 flex items-center gap-2'>
-                Datum: <span className='font-bold'>{trip.date}</span>
-            </h2>
-            <TripMap
-                startPos={[trip.from_lat, trip.from_long]}
-                endPos={[trip.to_lat, trip.to_long]}
-            />
+            <div className='flex items-center gap-2'>
+                <Calendar className='h-8 w-8 text-detail-yellow' />
+                <h2 className='text-h2  flex items-center gap-2'>
+                    Datum:{' '}
+                    <span className='font-bold'>
+                        {formatTime(trip.startTime)}
+                    </span>
+                </h2>
+            </div>
+
+            <div className='flex items-center gap-2'>
+                <DollarSign className='h-8 w-8 text-detail-yellow' />
+                <h2 className='text-h2   flex items-center gap-2'>
+                    Kostnad: <span className='font-bold'>{trip.cost}</span> kr
+                </h2>
+            </div>
+
+            <div className='h-[400px] flex justify-center items-center font-bold'>
+                <p>Karta över resan</p>
+            </div>
         </div>
     );
 }
--- services/frontend/src/app/actions/rental.js
+++ services/frontend/src/app/actions/rental.js
@@ -25,3 +25,51 @@ export async function rentBike(bikeId) {
 
     throw new Error(res.err || 'Renting bike failed!');
 }
+
+export async function getAllRentalUser(id) {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+
+    if (!token) {
+        throw new Error('Not authenticated');
+    }
+
+    const res = await fetch(`http://backend:3000/api/v1/rentals/user/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        cache: 'no-store',
+    });
+
+    if (res.ok) {
+        return res.json();
+    }
+
+    throw new Error(res.err || 'Fetching rentals failed!');
+}
+
+export async function getSingleRental(id) {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+
+    if (!token) {
+        throw new Error('Not authenticated');
+    }
+
+    const res = await fetch(`http://backend:3000/api/v1/rentals/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        cache: 'no-store',
+    });
+
+    if (res.ok) {
+        return res.json();
+    }
+
+    throw new Error(res.err || 'Fetching rental failed!');
+}
