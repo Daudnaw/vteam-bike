061aeac02f963f7bf82e2926bcd0a770d2a84e09
David Hildingsson <david.hildingsson@tierset.se>
2026-01-28 16:07:37 +0100

    fix: remove city button in user-app

--- services/frontend/components/map/AppMap.jsx
+++ services/frontend/components/map/AppMap.jsx
@@ -4,29 +4,6 @@ import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import UserLocation from './UserLocation';
 import LocateButton from './LocateButton';
-import CityButton from './CityButton';
-import { useState } from 'react';
-
-const zones = [
-    {
-        id: 1,
-        name: 'Göteborg',
-        latitude: 57.7089,
-        longitude: 11.9746,
-    },
-    {
-        id: 2,
-        name: 'Malmö',
-        latitude: 55.60498,
-        longitude: 13.0038,
-    },
-    {
-        id: 3,
-        name: 'Stockholm',
-        latitude: 59.3293,
-        longitude: 18.0686,
-    },
-];
 
 export default function AppMap({ zones }) {
     console.log('ZONES:', zones);
@@ -42,7 +19,6 @@ export default function AppMap({ zones }) {
                 attribution='&copy; OpenStreetMap contributors'
             />
             <LocateButton />
-            <CityButton zones={zones} />
             <UserLocation />
         </MapContainer>
     );
